3a6befdda5fd06c7e3d3be2a2f29256279362f83
Beese_h <marvh@beese.com>
2020-01-10 15:22:10 +0100

    Initial commit

---
+++ Basic_Gui/Window.py
@@ -0,0 +1,24 @@
+class Window():
+    def __init__(self):
+        # TODO: initialize needed variables
+        raise NotImplementedError
+    def launch(self):
+        #TODO: launch gui window
+        raise NotImplementedError
+        raise NotImplementedError
+
+    def exit(self):
+        #TODO: destroy all activity
+        raise NotImplementedError
+
+    def resize(self):
+        #TODO: update window after resize happened
+        raise NotImplementedError
+
+    def minimize(self):
+        #TODO: click on maximize
+        raise NotImplementedError
+
+    def maximize(self):
+        #TODO: click on minimize
+        raise NotImplementedError
---
+++ README.md
@@ -0,0 +1,2 @@
+# Intelligent-Editor-Environment
+ This Editor is a tool for reliable Textanalysis and Wordprediction. 
